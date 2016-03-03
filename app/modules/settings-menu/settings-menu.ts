1fbf09195a14404be51e577322652e3396403ac7
jaruesink <jaruesink@gmail.com>
2016-03-03 01:27:38 -0600

    able to close the menu now

--- app/modules/settings-menu/settings-menu.ts
+++ app/modules/settings-menu/settings-menu.ts
@@ -1,23 +1,44 @@
 "use strict";
 
-import {Component} from "angular2/core";
+import {Component, Renderer, Output, EventEmitter, HostListener} from "angular2/core";
 import {ROUTER_DIRECTIVES, RouteConfig, Route, RouterOutlet, RouterLink, Router, Location} from "angular2/router";
 import {AccountService} from "../../core/services/account-service/account.service";
 import {LoginService} from "../../core/services/login-service/login.service";
 import {GroupService} from "../../core/services/group-service/group.service";
-import {NgClass} from 'angular2/common';
 
 @Component({
     selector: "settings-menu",
     templateUrl: "modules/settings-menu/settings-menu.template.html",
-    directives: [ROUTER_DIRECTIVES, NgClass]
+    directives: [ROUTER_DIRECTIVES]
 })
 export class SettingsMenu {
     isOpen: boolean;
-    constructor(public accountService: AccountService, public loginService: LoginService, public groupService: GroupService, private router: Router) {
-        return;
+    event: any;
+    localEvent: any;
+    listener: any;
+
+    @HostListener("click", ["$event"])
+    onClick(event: any) {
+        this.localEvent = event;
+    }
+
+    constructor(public accountService: AccountService, public loginService: LoginService, public groupService: GroupService, private router: Router, private renderer: Renderer) {
+
     }
     toggleSettings() {
         this.isOpen = !this.isOpen;
+        if (this.isOpen) {
+            this.listener = this.renderer.listenGlobal('document', 'click', (event: any) => {
+                if (event.target.tagName !== this.localEvent.target.tagName) {
+                    event.preventDefault();
+                    this.handleOutsideClick();
+                }
+            });
+        } else {
+            this.listener();
+        }
+    }
+    handleOutsideClick() {
+        this.toggleSettings();
     }
 }
