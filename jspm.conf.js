038165ab1fd4a1b785da24304827bbdd2f5e5e98
jaruesink <jaruesink@gmail.com>
2016-03-04 16:58:51 -0600

    updates to app

--- jspm.conf.js
+++ jspm.conf.js
@@ -377,8 +377,7 @@ System.config({
     "npm:core-js@2.1.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-util-is@1.0.2": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0"
