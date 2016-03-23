ac6beabac19615dc270e841c733ae3c3755dfe6b
jaruesink <jaruesink@gmail.com>
2016-03-23 15:23:27 -0500

    slowly updating post list styles

---
+++ app/modules/actions-menu/comment-actions-menu.ts
@@ -0,0 +1,55 @@
+"use strict";
+
+import {Component, Renderer, Input, EventEmitter, HostListener} from "angular2/core";
+import {ROUTER_DIRECTIVES, RouteConfig, Route, RouterOutlet, RouterLink, Router, Location} from "angular2/router";
+import {AccountService} from "../../core/services/account-service/account.service";
+import {LoginService} from "../../core/services/login-service/login.service";
+import {GroupService} from "../../core/services/group-service/group.service";
+
+@Component({
+    selector: "comment-actions-menu",
+    templateUrl: "modules/actions-menu/comment-actions-menu.template.html",
+    directives: [ROUTER_DIRECTIVES]
+})
+export class CommentActionsMenu {
+    @Input() comment:any;
+    @Input() index:number ;
+    isOpen: boolean;
+    localEvent: any;
+    listener: any;
+
+    @HostListener("click", ["$event"])
+    onClick(event: any) {
+        this.localEvent = event;
+    }
+
+    constructor(public accountService: AccountService, public loginService: LoginService, public groupService: GroupService, public router: Router, private renderer: Renderer) {
+
+    }
+    toggleActions() {
+        this.isOpen = !this.isOpen;
+        console.log('Toggling the actions menu.');
+        if (this.isOpen) {
+            this.listener = this.renderer.listenGlobal('document', 'click', (event: any) => {
+                console.log(event);
+                console.log(this.localEvent);
+                if (event.target.tagName !== this.localEvent.target.tagName) {
+                    this.handleClick(event);
+                } else if ( event.target.className === "action-item" ) {
+                  this.handleClick(event);
+                }
+            });
+        } else {
+            this.listener();
+        }
+    }
+    handleClick(event:any) {
+        this.toggleActions();
+    }
+    favoriteGroup(index:number) {
+        this.groupService.groups[index].starred = !this.groupService.groups[index].starred;
+    }
+    editPost() {
+        return;
+    }
+}
--- app/modules/comment-list/comment-list.ts
+++ app/modules/comment-list/comment-list.ts
@@ -2,12 +2,13 @@
 
 import {Component, Input, Output, EventEmitter} from "angular2/core";
 import {ROUTER_DIRECTIVES, Router, RouterLink} from "angular2/router";
+import {CommentActionsMenu} from "../actions-menu/comment-actions-menu";
 import {GroupService} from "../../core/services/group-service/group.service";
 
 @Component({
     selector: "comment-list",
     templateUrl: "../modules/comment-list/comment-list.template.html",
-    directives: [RouterLink]
+    directives: [RouterLink, CommentActionsMenu]
 })
 export class CommentList {
     @Input() commentList: Array<any>;
